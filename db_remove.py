4cdbe8735df17ea62c661ec6f89c79eae7d33fc9
Enzo <vieira.enzof@gmail.com>
2022-10-04 00:48:31 -0300

    teste

--- db_remove.py
+++ db_remove.py
@@ -4,9 +4,12 @@ conn = sqlite3.connect("pf.db")
 c = conn.cursor()
 
 a = input("quem deletar: ")
+
 c.execute('''
 DELETE FROM alunos WHERE nome = ? ''', (a,) )
 
 b = c.execute("SELECT * FROM alunos").fetchall()
-print(b)
+
+
+
 conn.commit()
